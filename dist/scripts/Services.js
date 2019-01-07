b9c90c67dea9a1cdf58bc8c270af22ba74d0ee8f
Aaron Moreno <info@aaronmoreno.co>
2019-01-06 23:37:24 -0700

    fixed media queries and added Menu w/ JSON

---
+++ dist/scripts/Services.js
@@ -0,0 +1,142 @@
+//Menu Items
+const services = {
+  wax1: {
+    name: "brow shaping",
+    details: " ",
+    price: 25,
+    featured: false
+  }, 
+  wax2:{
+    name: "brow + lip",
+    details: " ",
+    price: 30,
+    featured: false
+  },
+  wax3:{
+    name: "ace (brow, lip, cheek, chin)",
+    details: " ",
+    price: 40,
+    featured: false
+  },
+  wax4:{
+    name: "brow shaping",
+    details: " ",
+    price: 25,
+    featured: false
+  },
+  wax5:{
+    name: "half leg",
+    details: " ",
+    price: 40,
+    featured: false
+  },
+  wax6:{
+    name: "full leg",
+    details: " ",
+    price: 60,
+    featured: false
+  },
+  wax7:{
+    name: "bikini",
+    details: " ",
+    price: 35,
+    featured: false
+  },
+  wax8:{
+    name: "brazilian",
+    details: " ",
+    price: 50,
+    featured: false
+  },
+  wax9:{
+    name: "underarm",
+    details: " ",
+    price: 20,
+    featured: false
+  },
+  wax10:{
+    name: "arm",
+    details: " ",
+    price: 40,
+    featured: false
+  },
+  wax11:{
+    name: "back",
+    details: " ",
+    price: 50,
+    featured: false
+  },
+  wax12:{
+    name: "chest",
+    details: " ",
+    price: 50,
+    featured: false
+  },
+
+/*Enhancements */
+  waxEnhancement1:{
+    name: "lash tint",
+    details: "boost color, semi-permanent, immediate results",
+    price: 20,
+    featured: false
+  },
+  waxEnhancement2:{
+    name: "brow tint",
+    details: "boost thickness and color, immediate results",
+    price: 20,
+    featured: false
+  },
+  waxEnhancement3:{
+    name: "brow tint add-on",
+    details: "add to brow shape to enhance thickness",
+    price: 10,
+    featured: false
+  },
+
+  /*Treatments */
+  treatment1:{
+    name: "mini facial",
+    details: "cleanse, exfoliate, nourish, relax, customizable",
+    price: 60,
+    featured: false
+  },
+  treatment2:{
+    name: "microdermabrasion",
+    details: "renew skin tone + texture, brighten, treat",
+    price: 90,
+    featured: false
+  },
+  treatment3:{
+    name: "progressive peel",
+    details: "refining, removes outer layers of skin, skin may have mild sloughing, treatment can be repeated three to four weeks apart",
+    price: 65,
+    featured: false
+  },
+  treatment4:{
+    name: "mid depth peel",
+    details: "rejuvenating, targets deeper layers of the skin, treatment can be repeated monthly",
+    price: 85,
+    featured: false
+  },
+  treatment5:{
+    name: "deep peel",
+    details: "restoring, reaches deepest epidermal layers of the skin, peels in large segments occurring three to four days after treatment, recovery lasts seven to ten days, can be repeated four times per year",
+    price: 100,
+    featured: false
+  },
+  treatment6:{
+    name: "micro peel",
+    details: "combo of microdermabrasion and peel, customizable, downtime depending on peel used",
+    price: 125,
+    featured: false
+  },
+  treatment7:{
+    name: "dermaplaning",
+    details: "light exfoliation, removal of peach fuzz, results are immediate, no downtime",
+    price: 50,
+    featured: false
+  },
+
+};
+
+export default services;
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,32 +1,20 @@
+//import {services} from './Services';
 const mainNav = document.querySelector(".main-nav");
 let topOfNav = mainNav.offsetTop;
-const aboutHeader = document.querySelector(".about h2");
-const contactButton = document.querySelector(".contact-button");
-const portStrike = document.querySelector(".strike");
-const portButtons = Array.from(document.querySelectorAll(".port-options h3"));
-let details = document.createElement("div");
-let textBoxOutput = document.querySelector(".text-box-icon");
-const mainIcon = document.querySelector(".main-icon-clickable");
 const mainHeader = document.querySelector(".main-top");
-const aaronSayings = [
-  "Hey, how's it going?",
-  "Click Below to say hi!",
-  "What's your favororite food?"
-];
-let i = 0;
-let time = new Date();
-let date = parseFloat(time.getHours() + "." + time.getMinutes());
+const serviceTitle = document.querySelector(".menu h1");
 
-//changes main color of background
-window.onload = function() {
-  if (date <= 10) {
-    mainHeader.classList.toggle("morning");
-  } else if (date >= 11) {
-    mainHeader.classList.toggle("afternoon");
-  } else {
-    mainHeader.classList.toggle("night");
-  }
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(xhttp.responseText);
+    }
 };
+xhttp.open("GET", "services.json", true);
+xhttp.send();
+
+
+
 function fixNav() {
   if (window.scrollY >= topOfNav) {
     document.body.style.paddingTop = mainNav.offsetHeight + "px";
@@ -49,66 +37,13 @@ let isInViewport = function(elem) {
   );
 };
 
-function inView(event) {
-  if (isInViewport(aboutHeader)) {
-    aboutHeader.classList.add("bounceInUp");
-    contactButton.classList.add("bounceIn");
-  }
-}
-function portfolioButton() {
-  details.classList.add("details");
-  const portfolioDetails = document.querySelector(".port-options");
-  portfolioDetails.appendChild(details);
-
-  if (this.innerHTML == "Websites") {
-    details.innerHTML = `
-      <a href="https://littlebearcoffeeco.com" target="_blank">
-          <img src="images/portfolio/littlebear.png" alt ="little bear coffee">
-        </a>
-    `;
-  } else if (this.innerHTML == "Javascript") {
-    details.innerHTML = `
-    <div class="portfolio-images">
-      <a href="http://aaronmoreno.co/tip-calc" target="_blank">
-        <img src="images/portfolio/tip.png" alt ="tip calculator">
-      </a>
-      <p>Tip Calculator</p>
-    </div>
-
-    <div class="portfolio-images">
-      <a href="http://aaronmoreno.co/tip-cal" target="_blank">
-        <img src="images/portfolio/calculator.png" alt ="tip calculator">
-      </a>
-      <p>Calculator</p>
-    </div>
-    
-    `;
-  } else {
-    details.innerHTML = `
-            <h2>${this.innerHTML} coming soon...</h2>
-    `;
-  }
-}
-
 //Contact Form Will Come Soon
 function formSubmit(e) {
   e.preventDefault();
   console.log("this works");
 }
 
-function textBoxFunc() {
-  if (i >= aaronSayings.length) {
-    i = 0;
-  } else {
-    textBoxOutput.innerHTML = aaronSayings[i];
-    i++;
-  }
-}
 
 window.addEventListener("scroll", fixNav);
 window.addEventListener("scroll", inView);
-portButtons.forEach(button =>
-  button.addEventListener("click", portfolioButton)
-);
 document.querySelector(".submit").addEventListener("click", formSubmit);
-mainIcon.addEventListener("click", textBoxFunc);
